6590c0790921adefad846c201a04fd5f9e26f441
Am Pythonista <dihfahsihm@gmail.com>
2022-08-30 17:27:28 +0300

    restructure the project directory

---
+++ test-cases/auth.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+def credentials():
+  browser = webdriver.Chrome('C:/Users/Dev/Desktop/chromedriver/chromedriver.exe')
+  browser.get("http://127.0.0.1:8000/sa/login/?next=/sa/") 
+  time.sleep(1)
+  username = browser.find_element_by_id("id_username")
+  password = browser.find_element_by_id("id_password")
+  username.send_keys("admin@gmail.com")
+  password.send_keys("1234")
+  login_attempt = browser.find_element_by_xpath("//*[@type='submit']")
+  login_attempt.submit()
