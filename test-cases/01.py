517e6d09af737d6d3991a37b3f3dadaf8bf2efe2
Am Pythonista <dihfahsihm@gmail.com>
2022-09-07 02:37:43 +0300

    update code

--- test-cases/01.py
+++ test-cases/01.py
@@ -1,18 +1,18 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 import time
 browser = webdriver.Chrome('C:/Users/Dev/Desktop/chromedriver/chromedriver.exe')
 browser.get("http://127.0.0.1:8000/sa/login/?next=/sa/") 
 time.sleep(1)
-username = browser.find_element_by_id("id_username")
-password = browser.find_element_by_id("id_password")
+username = browser.find_element(By.ID,"id_username")
+password = browser.find_element(By.ID,"id_password")
 username.send_keys("admin@gmail.com")
 password.send_keys("1234")
-login_attempt = browser.find_element_by_xpath("//*[@type='submit']")
+login_attempt = browser.find_element(By.XPATH, "//*[@type='submit']")
 login_attempt.submit()
 
-element = browser.find_element_by_tag_name('h1')
+element = browser.find_element(By.TAG_NAME,'h1')
 if element.text == 'Welcome!':
-    print("Pass")
+    print("Landing page Passed the test")
 else:
     print("Fail")
